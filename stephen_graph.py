4a825683409999ba6b2b0a9a2bcd989713af5a3a
Stephen Thomas <mewzyk@gmail.com>
2017-12-25 12:44:06 -0800

    Finished refactoring the codebase to use an adjacency list instead of an adjaceny matrix.

--- stephen_graph.py
+++ stephen_graph.py
@@ -5,18 +5,23 @@ Basic implementation of an undirected graph
 Github Link: https://github.com/Mewzyk/stephen_AI.git
 """
 
+class Node:
+    def __init__(self, key, neighbors = []):
+        self.key = key
+        self.neighbors = set(neighbors)
+
 class Graph:
     def __init__(self, graph_dict = None):
         if graph_dict == None:
             graph_dict = {}
-        self.__graph_dict = graph_dict
+        self.graph_dict = graph_dict
 
     def vertices(self):
-        return list(self.__graph_dict.keys())
+        return list(self.graph_dict.keys())
 
     def add_vertex(self, vertex):
-            if vertex not in self.__graph_dict:
-                self.__graph_dict[vertex] = []
+            if vertex not in self.graph_dict:
+                self.graph_dict[vertex] = Node(vertex)
 
     def add_edge(self, edge):
         edge = set(edge)
@@ -24,17 +29,20 @@ class Graph:
             left = edge.pop()
             right = edge.pop()
 
-            if left in self.__graph_dict and right in self.__graph_dict:
-                self.__graph_dict[left].append(right)
-                self.__graph_dict[right].append(left)
+            if left in self.graph_dict and right in self.graph_dict:
+                self.graph_dict[left].neighbors.add(self.graph_dict[right])
+                self.graph_dict[right].neighbors.add(self.graph_dict[left])
 
     def print_graph(self):
         print('Printing Vertices: ')
-        print(self.__graph_dict.keys(), '\n')
+        print(self.graph_dict.keys(), '\n')
 
-        print('Printing Edges: \n')
-        for vertex in self.__graph_dict.keys():
-            print(vertex, '<---->', self.__graph_dict[vertex])
+        print('Printing Edges: ')
+        for key in self.graph_dict.keys():
+            output = []
+            for edge in self.graph_dict[key].neighbors:
+                output.append(edge.key)
+            print(key, '<-->', output)
 
 if __name__ == "__main__":
     main_graph = Graph()
