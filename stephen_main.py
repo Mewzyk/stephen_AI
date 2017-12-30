b44e1b8d1c4da7de4f714a7f46df3d4e72fc683b
Stephen Thomas <mewzyk@gmail.com>
2017-12-29 17:17:53 -0800

    Trivial Uopdate

--- stephen_main.py
+++ stephen_main.py
@@ -3,18 +3,22 @@ from stephen_dfs import dfs
 
 if __name__ == "__main__":
     main_graph = Graph()
-    vertices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    edges = [['a', 'b'], ['b', 'c'], ['c', 'd'], ['d', 'e'], ['e', 'f'], ['f', 'g'], ['g', 'h']]
+
+    vertices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+    edges = [['a', 'b'], ['a', 'c'], ['b', 'd'], ['d', 'h'], ['b', 'e'], ['c', 'f'], ['c', 'g'], ['e', 'i']]
+
     for vertex in vertices:
         main_graph.add_vertex(vertex)
 
     for edge in edges:
         main_graph.add_edge(edge)
 
+    print('==================================')
     main_graph.print_graph()
 
-    start = main_graph.graph_dict['e']
-    end = main_graph.graph_dict['h']
 
-    print('Printing Path: ')
+    start = main_graph.graph_dict['i']
+    end = main_graph.graph_dict['f']
+
+    print('\nPrinting Path: ')
     print(dfs(start, end))
