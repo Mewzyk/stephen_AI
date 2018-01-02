1ec2b0a1f094d1b169364e8c68605f1896955aba
Stephen Thomas <mewzyk@gmail.com>
2018-01-01 16:37:13 -0800

    Started developing data pipeline python code

--- graph_code/stephen_data_management.py
+++ graph_code/stephen_data_management.py
@@ -4,3 +4,13 @@ Date Created: 1 December 2018
 Pipeline between D3.js and Graph framework
 Github Link: https://github.com/Mewzyk/stephen_AI.git
 """
+
+import json
+
+with open("../visualization_code/miserbales_dataset.json", "r") as f:
+    content = f.read()
+
+my_dict = json.loads(content)
+
+for key in my_dict.keys():
+    print(key)
